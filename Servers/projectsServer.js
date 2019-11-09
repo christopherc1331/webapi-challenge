a72b8fcb19a0d5f37d6dc8cb5e3ae339f5ccb3da
Chris Cruz <christopherc1331@gmail.com>
2019-11-08 20:08:53 -0800

    Finished project routing

--- Servers/projectsServer.js
+++ Servers/projectsServer.js
@@ -2,6 +2,39 @@ const express = require("express");
 const router = express.Router();
 const projectDb = require("../data/helpers/projectModel.js");
 
-router.get("/");
+router.get("/", (req, res) => {
+  projectDb
+    .get()
+    .then(projects => res.status(200).json({ success: true, projects }))
+    .catch(err => res.status(500).json({ success: false, err }));
+});
+
+router.post("/", (req, res) => {
+  const newPost = req.body;
+
+  projectDb
+    .insert(newPost)
+    .then(addedPost => res.status(201).json({ success: true, addedPost }))
+    .catch(err => res.status(500).json({ success: false, err }));
+});
+
+router.put("/:id", (req, res) => {
+  const updatedPostInfo = req.body;
+  const { id } = req.params;
+
+  projectDb
+    .update(id, updatedPostInfo)
+    .then(updatedPost => res.status(202).json({ success: true, updatedPost }))
+    .catch(err => res.status(500).json({ success: false, err }));
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  projectDb
+    .remove(id)
+    .then(() => res.status(200).end())
+    .catch(err => res.status(500).json({ success: false, err }));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const actionServer = require("./Servers/actionServer.js"),
 // file imports //
 
 const server = express();
-
+server.use(express.json());
 server.use(helmet());
 server.use("/api/actions", actionServer);
 server.use("/api/projects", projectsServer);
